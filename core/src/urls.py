9c70e00abd6c922c8946a70ca45de94d110ea794
Hong Vo <vo.duc.hong94@gmail.com>
2017-09-04 18:58:33 +0700

    core : add theme to admin page

--- core/src/urls.py
+++ core/src/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls import include
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
+	url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
     url(r'^admin/', admin.site.urls),
 ]
